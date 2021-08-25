54a9aea786bdc1c14f91c7831f5391774f964000
uglyonlytoday <superjang3357@naver.com>
2021-08-25 15:01:10 +0900

    update 함수 오류 해결

--- todo/serializers.py
+++ todo/serializers.py
@@ -7,8 +7,3 @@ class TodoSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Todo
 		fields = '__all__'
-
-	def update(self, instance:Todo, validated_data):
-		instance.is_done = validated_data('is_done')
-		instance.save()
-		return instance
