a7e4ac500f63d7be3410b15152c67d4f1d274396
uglyonlytoday <superjang3357@naver.com>
2021-09-01 20:19:29 +0900

    TodoSerializer validation 적용 title이 50자가 넘어갈 시 오류

--- todo/serializers.py
+++ todo/serializers.py
@@ -1,5 +1,3 @@
-from django.db import models
-from django.db.models import fields
 from rest_framework import serializers
 from .models import Todo
 
@@ -7,3 +5,7 @@ class TodoSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Todo
 		fields = '__all__'
+	def validate_title(self, value):
+		if len(value) > 50:
+			raise serializers.ValidationError('Title is too long!')
+		return value
