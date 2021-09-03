fa17eaddd177c1ef6e341c24daa755bdf4555848
uglyonlytoday <superjang3357@naver.com>
2021-09-03 22:12:58 +0900

    Type Annotation 교정 HttpRequest -> Request Request는 HttpRequest의 DRF 확장 판임

--- todo/views.py
+++ todo/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from rest_framework import status
-from todo.serializers import TodoSerializer
-from .models import Todo
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.request import HttpRequest
+from rest_framework.request import Request
+from .models import Todo
+from .serializers import TodoSerializer
 
 class TodoView(APIView):
-	def get(self, request:HttpRequest):
+	def get(self, request:Request):
 		todos = Todo.objects.all()
 		serializer = TodoSerializer(todos, many = True)
 		return Response(serializer.data)
-	def post(self, request:HttpRequest):
+	def post(self, request:Request):
 		serializer = TodoSerializer(data=request.data)
 		if serializer.is_valid():
 			serializer.save()
@@ -20,12 +20,12 @@ class TodoView(APIView):
 		return Response(serializer.errors)
 
 class TodoDetailView(APIView):
-	def get(self, request:HttpRequest, pk:int):
+	def get(self, request:Request, pk:int):
 		todo = get_object_or_404(Todo, pk=pk)
 		serializer = TodoSerializer(todo)
 		return Response(serializer.data)
 	
-	def put(self, request:HttpRequest, pk:int):
+	def put(self, request:Request, pk:int):
 		todo = get_object_or_404(Todo, pk=pk)
 		serializer = TodoSerializer(instance=todo, data=request.data)
 		if serializer.is_valid():
@@ -33,14 +33,14 @@ class TodoDetailView(APIView):
 			return Response(serializer.data)
 		return Response(serializer.errors)
 
-	def delete(self, request:HttpRequest, pk:int):
+	def delete(self, request:Request, pk:int):
 		todo = get_object_or_404(Todo, pk = pk)
 		todo.delete()
 		return Response(status=status.HTTP_204_NO_CONTENT)
 
 # Create your views here.
 @api_view(['GET', 'POST'])
-def todos(request:HttpRequest):
+def todos(request:Request):
 	if request.method == 'GET':
 		queryset = Todo.objects.all()
 		serializer = TodoSerializer(queryset, many=True)
@@ -54,7 +54,7 @@ def todos(request:HttpRequest):
 			return Response(serializer.errors)
 
 @api_view(['GET', 'PUT', 'DELETE'])
-def todo_detail(request:HttpRequest, pk):
+def todo_detail(request:Request, pk):
 	if request.method == 'GET':
 		todo = get_object_or_404(Todo, pk=pk)
 		serializer = TodoSerializer(todo)
